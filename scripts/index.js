99a12ab5fda797389505f0123910f6fe86dbf0f5
PashaTai <ptayanko@yandex.ru>
2024-02-11 19:17:10 +0300

    Второй коммит ПР-5 после прохода prettier

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,26 @@
+const cardTemplate = document.querySelector("#card-template").content;
 
-const cardTemplate = document.querySelector('#card-template').content;
+function addCard(cardData, functionDeleteCardCallBack) {
+  const cardElement = cardTemplate.querySelector("li").cloneNode(true);
 
+  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardElement.querySelector(".card__image").src = cardData.link;
+  cardElement.querySelector(".card__image").alt = `На фото ${cardData.name}`;
 
-function addCard (cardData, functionDeleteCardCallBack) {
-  const cardElement = cardTemplate.querySelector('li').cloneNode(true);
-
-  cardElement.querySelector('.card__title').textContent = cardData.name;
-  cardElement.querySelector('.card__image').src = cardData.link;
-  cardElement.querySelector('.card__image').alt = `На фото ${cardData.name}`;
-
-  const deleteCardButton = cardElement.querySelector('.card__delete-button');
-  deleteCardButton.addEventListener('click', () => {functionDeleteCardCallBack(cardElement)} );
+  const deleteCardButton = cardElement.querySelector(".card__delete-button");
+  deleteCardButton.addEventListener("click", () => {
+    functionDeleteCardCallBack(cardElement);
+  });
 
   return cardElement;
-} 
+}
 
 const deleteCard = (cardElement) => {
   cardElement.remove();
-}
+};
 
-const cardList = document.querySelector('.places__list');
+const cardList = document.querySelector(".places__list");
 
 initialCards.forEach((cardData) => {
-    
   cardList.append(addCard(cardData, deleteCard));
-})
-
-
-
-
-
+});
