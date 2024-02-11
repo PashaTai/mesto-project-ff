3a6f37fb30ad4c42c266a6c13c603dac94b8d504
PashaTai <ptayanko@yandex.ru>
2024-02-12 00:12:08 +0300

    После ревью правки

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,11 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 function addCard(cardData, functionDeleteCardCallBack) {
   const cardElement = cardTemplate.querySelector("li").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
 
   cardElement.querySelector(".card__title").textContent = cardData.name;
-  cardElement.querySelector(".card__image").src = cardData.link;
-  cardElement.querySelector(".card__image").alt = `На фото ${cardData.name}`;
+  cardImage.src = cardData.link;
+  cardImage.alt = `На фото ${cardData.name}`;
 
   const deleteCardButton = cardElement.querySelector(".card__delete-button");
   deleteCardButton.addEventListener("click", () => {
